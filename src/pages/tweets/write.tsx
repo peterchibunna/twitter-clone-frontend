1a177df2a654940bac5337d5b30aceb6821e0f04
Peter Chibunna <peterchibunna@gmail.com>
2024-06-21 08:01:06 +0100

    more users styling: just a bit

--- src/pages/tweets/write.tsx
+++ src/pages/tweets/write.tsx
@@ -12,7 +12,7 @@ function getSelectValues(select: HTMLSelectElement) {
   const options = select && select.options;
   let opt;
 
-  for (let i=0, iLen=options.length; i<iLen; i++) {
+  for (let i = 0, iLen = options.length; i < iLen; i++) {
     opt = options[i];
 
     if (opt.selected) {
@@ -22,14 +22,14 @@ function getSelectValues(select: HTMLSelectElement) {
   return result;
 }
 
-export function WriteTweet({isLoading, setLoading, setIsOpen}: any) {
+export function WriteTweet({setLoading, setIsOpen}: any) {
   const dispatch = useAppDispatch();
   const [tweet, setTweet] = useState('');
   const {
     data: otherUsers,
-    isLoading: usersLoading,
-    error,
-    mutate
+    // isLoading: usersLoading,
+    // error,
+    // mutate
   } = useSWR(`${API_SERVER_URL}/api/v1/person/`, fetcher, {
     keepPreviousData: true,
   });
@@ -47,7 +47,7 @@ export function WriteTweet({isLoading, setLoading, setIsOpen}: any) {
         body: JSON.stringify({
           text: tweet,
           by: `/api/v1/person/${auth?.user?.id}/`,
-          shared_with: sharedWithIds.map((item:string) => {
+          shared_with: sharedWithIds.map((item: string) => {
             return `/api/v1/person/${item}/`
           }),
         }),
@@ -80,17 +80,24 @@ export function WriteTweet({isLoading, setLoading, setIsOpen}: any) {
           className="h-36 p-3 outline-none border-gray-300 w-full resize-none border rounded-lg"
           placeholder="Write your tweet here"></textarea>
         <div className="flex justify-between mt-2 text-xs">{tweet.length} character{tweet.length != 1 && 's'}</div>
-        <div className="flex justify-between mt-2">
-          <p className="italic font-bold text-amber-700">Share with others: <select multiple={true} id={'shared_with_ids'}>
-            {otherUsers?.objects?.map((user: any) => {
-                if (user.id !== auth?.user?.id) {
-                  return (
-                    <option key={user.username} value={user.id}>{user.username}</option>
-                  );
+        <div className="flex justify-between mt-2 items-center">
+          <p className="italic text-gray-600 mb-0 items-center flex">
+            <span>Share with others: </span>
+            <select
+              size={1}
+              className={'ml-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300' +
+                ' placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'}
+              multiple={true} id={'shared_with_ids'}>
+              {otherUsers?.objects?.map((user: any) => {
+                  if (user.id !== auth?.user?.id) {
+                    return (
+                      <option key={user.username} value={user.id}>{user.username}</option>
+                    );
+                  }
                 }
-              }
-            )}
-          </select></p>
+              )}
+            </select>
+          </p>
           <button
             onClick={postTweet}
             disabled={tweet.length <= 1}
@@ -98,6 +105,7 @@ export function WriteTweet({isLoading, setLoading, setIsOpen}: any) {
             Post Tweet
           </button>
         </div>
+        <div className={'block text-xs'}>Hold down control to select/deselect items</div>
       </div>
     </div>
   );
